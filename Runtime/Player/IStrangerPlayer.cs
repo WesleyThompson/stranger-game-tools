b3bb338b78872155d99a7d64e131e7d22800c27e
Wesley Thompson <a.wes.thompson@gmail.com>
2026-03-07 00:34:39 -0500

    Removed isGrounded and added canMove to IStrangerPlayer

--- Runtime/Player/IStrangerPlayer.cs
+++ Runtime/Player/IStrangerPlayer.cs
@@ -3,9 +3,9 @@ namespace StrangerGameTools.Player
     public interface IStrangerPlayer
     {
         bool CanLook { get; set; }
+        bool CanMove { get; set; }
         bool CanJump { get; set; }
         bool IsControllable { get; set; }
-        bool IsGrounded { get; set; }
         float MoveSpeed { get; set; }
 
         void Setup();
--- Runtime/Player/StrangerPlayerBase.cs
+++ Runtime/Player/StrangerPlayerBase.cs
@@ -9,9 +9,12 @@ namespace StrangerGameTools.Player
     [DisallowMultipleComponent]
     public abstract class StrangerPlayerBase : MonoBehaviour, IStrangerPlayer
     {
-        [Header("Player Settings")]
+        [Header("Base Player Settings")]
         [SerializeField]
         private bool _canLook = true;
+
+        [SerializeField]
+        private bool _canMove = true;
 
         [SerializeField]
         private bool _canJump = true;
@@ -20,15 +23,12 @@ namespace StrangerGameTools.Player
         private bool _isControllable = true;
 
         [SerializeField]
-        private bool _isGrounded;
-
-        [SerializeField]
         private float _moveSpeed = 5f;
 
         public bool CanLook { get => _canLook; set => _canLook = value; }
+        public bool CanMove { get => _canMove; set => _canMove = value; }
         public bool CanJump { get => _canJump; set => _canJump = value; }
         public bool IsControllable { get => _isControllable; set => _isControllable = value; }
-        public bool IsGrounded { get => _isGrounded; set => _isGrounded = value; }
         public float MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
 
         /// <summary>
